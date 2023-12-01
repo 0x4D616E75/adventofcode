5a739455d63c454536dfe29329f16d86c7461953
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2023-12-01 11:25:47 +0100

    solve P2 of 01/2023

--- 2023/Day01/Solution.cs
+++ 2023/Day01/Solution.cs
@@ -11,25 +11,58 @@ namespace AdventOfCode.Y2023.Day01;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        var calibrationValues = ExtractCalibrationValues(input);
+        var calibrationValues = ExtractCalibrationValues(input, regexPartOne);
         return calibrationValues.Sum();
     }
 
     public object PartTwo(string input) {
-        return 0;
+        var calibrationValues = ExtractCalibrationValues(input, regexPartTwo);
+        return calibrationValues.Sum();
     }
-    Regex regex = new (@"\d");
-    private IEnumerable<int> ExtractCalibrationValues(string input)
+
+
+    const string regexPartOne = @"\d";
+    const string regexPartTwo = @"\d|one|two|three|four|five|six|seven|eight|nine";
+    private IEnumerable<int> ExtractCalibrationValues(string input, string regexPattern)
     {
         var calibrationValues = new List<int>();
         var lines = input.Split('\n');
         foreach(var line in lines)
         {
-            var matches = regex.Matches(line);
-            var first = int.Parse(matches.FirstOrDefault().Value);
-            var last = int.Parse(matches.LastOrDefault().Value);
-            calibrationValues.Add(int.Parse($"{first}{last}"));
+            // For Part 2 the search need to be done from right to left to find digits as word when last and first letter are same.
+            // e.g. oneight --> None finds "one" and RightToLeft finds "eight"
+            var first = ParseToInt(Regex.Match(line, regexPattern, RegexOptions.None).Value);
+            var last = ParseToInt(Regex.Match(line, regexPattern, RegexOptions.RightToLeft).Value);
+            calibrationValues.Add(first * 10 + last);
         }
         return calibrationValues;
+    }
+
+    private int ParseToInt(string value)
+    {
+        switch (value)
+        {
+            case "one":
+                return 1;
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+            default:
+                return int.Parse(value);
+        }
+        
     }
 }
