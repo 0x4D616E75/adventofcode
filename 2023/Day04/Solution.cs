26c176db2fc2e7c187e0e8e8dc7da2a8cfddf119
0x4D616E75 <0x4d616e75@elektronische-nachricht.de>
2023-12-04 20:34:35 +0100

    Solve 04/23

---
+++ 2023/Day04/Solution.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Text;
+using AngleSharp.Common;
+
+namespace AdventOfCode.Y2023.Day04;
+
+[ProblemName("Scratchcards")]
+class Solution : Solver {
+
+    public object PartOne(string input) {
+        var cards = ParseCards(input);
+        return cards.Select(i => i.Points).Sum();
+    }
+
+    public object PartTwo(string input) {
+        var cards = ParseCards(input);
+        var copies = cards.SelectMany(i => i.GetCopies(cards));
+        return cards.Count() + copies.Count();
+    }
+
+    record Card(int Number, IEnumerable<int> WinningNumbers, IEnumerable<int> MyNumbers)
+    {
+        public int MatchCount => MyNumbers.Intersect(WinningNumbers).Count(); 
+        public int Points => (int)(MatchCount == 0 ? 0 : Math.Pow(2, MatchCount-1));
+
+        public IEnumerable<Card> GetCopies(IEnumerable<Card> cards)
+        {
+            var copies = new List<Card>();
+            for(var i = 0; i < MatchCount; i++)
+            {
+                var idx = Number + i;
+                if(idx < cards.Count())
+                {
+                    var copy = cards.ElementAt(idx);
+                    copies.Add(copy);
+                    copies.AddRange(copy.GetCopies(cards));
+                }
+            }
+            return copies;
+        }
+    }
+
+    IEnumerable<Card> ParseCards(string input)
+    {
+        var cards = new List<Card>();
+        var lines = input.Split('\n');
+        foreach(var line in lines)
+        {
+            var card = Regex.Match(line, @"Card\s+(\d+):(.+)\|(.+)");
+            var number = int.Parse(card.Groups[1].Value);
+            var winningNumbers = Regex.Matches(card.Groups[2].Value, @"\d+").Select(i => int.Parse(i.Value));
+            var myNumbers = Regex.Matches(card.Groups[3].Value, @"\d+").Select(i => int.Parse(i.Value));
+            cards.Add(new (number, winningNumbers, myNumbers));
+        }
+        return cards;   
+    }
+   
+}
